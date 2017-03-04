586a0e289c8df2f571c095ca085ff44ac3850ef9
Georgi Naumov <gonaumov@gmail.com>
2017-03-05 00:36:04 +0200

    Add two way data binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class Hero {
              <h2>{{hero.name}} details!</h2>
              <div><label>id: </label>{{hero.id}}</div>
              <div><label>name: </label>
-             <input value="{{hero.name}}" placeholder="name">
+             <input [(ngModel)]="hero.name" placeholder="name">
              </div>`
 })
 

80ef9fb1ffd86c7e67442200edd1a71dedcb0309
Georgi Naumov <gonaumov@gmail.com>
2017-03-13 00:17:50 +0200

    Implement create and delete of heroes. Also delete unnecessary semicolon.

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -12,22 +12,39 @@ import { Router } from '@angular/router';
 })
 
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
-  selectedHero: Hero;
+  heroes: Hero[]
+  selectedHero: Hero
   constructor(private router: Router,
               private heroService: HeroService) {};
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-  };
+  }
   ngOnInit(): void {
     this.getHeroes();
-  };
+  }
   getHeroes(): void {
     this.heroService.getHeroes().then(
       heroes => this.heroes = heroes
     );
-  };
+  }
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
   }
 }
